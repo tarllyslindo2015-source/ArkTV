efaa709ef2d8840cd3fc1064a5cbb38f085841b0
tarllyslindo2015-source <tarllyslindo2015@gmail.com>
2025-09-05 15:34:54 -0300

    Update ArkTV.sh

--- ArkTV.sh
+++ ArkTV.sh
@@ -15,7 +15,7 @@ set -euo pipefail
 CURR_TTY="/dev/tty1"
 MPV_SOCKET="/tmp/mpvsocket"
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-JSON_URL="https://raw.githubusercontent.com/AeolusUX/ArkTV/refs/heads/main/channels/channels.json"
+JSON_URL="https://raw.githubusercontent.com/tarllyslindo2015-source/ArkTV/refs/heads/main/channels/channels.json"
 JSON_FILE="/tmp/channels.json"  # Temporary file for channels list
 
 # --- Functions ---
